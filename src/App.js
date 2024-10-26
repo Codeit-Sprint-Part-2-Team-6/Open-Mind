143e00f3fa04226d43c2a8ea69e1867e75bdabb9
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-26 19:17:53 +0900

    feat: FeedDetailPage 컴포넌트 생성
    - 기본 컴포넌트 생성
    - 추후 헤더와 컨테이너 등을 구현해 추가 예정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FeedDetailPage from './pages/FeedDetail/FeedDetailPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route />
+        <Route path='/post' element={<FeedDetailPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/FeedDetail/FeedDetailPage.jsx
@@ -0,0 +1,3 @@
+function FeedDetailPage() {}
+
+export default FeedDetailPage;
