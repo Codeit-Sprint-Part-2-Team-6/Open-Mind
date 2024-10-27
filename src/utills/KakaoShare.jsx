54d08da18c5c5a7dbb4c9be18c381e0e46ad2bb3
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-27 15:55:24 +0900

    feat: 카카오톡 공유 기능 구현
    - Kakao developers 에서 앱 생성
    - 앱에서 메시지 템플릿 작성
    - 버튼을 구현하여 공유 링크를 보내는 것까지 구현
    - 아직 배포가 되지 않았고, 개별 페이지 api 구현 이전이므로 현재 연결
      링크는 / 라서 추후 수정 필요

---
+++ src/utills/KakaoShare.jsx
@@ -0,0 +1,18 @@
+const { Kakao } = window;
+
+export const shareKakao = () => {
+  if (!Kakao.isInitialized()) {
+    Kakao.cleanup();
+    Kakao.init('e69f2c4b1ed944c523867388a90b8bab');
+  }
+
+  Kakao.Link.sendCustom({
+    templateId: 113569,
+    templateArgs: {
+      PROFILE:
+        'https://helpx.adobe.com/content/dam/help/en/photoshop/using/quick-actions/remove-background-before-qa1.png', // 테스트 이미지
+      TITLE: '오픈 마인드 피드',
+      DESC: '피드 소개글',
+    },
+  });
+};
