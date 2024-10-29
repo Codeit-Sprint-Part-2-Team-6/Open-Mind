93b0bf78081c194c502bc056c0ce4a7ba3b6a0f0
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-29 11:04:55 +0900

    feat : App.js에 ThemeProvider, theme, GlobalStyles, Helmet적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyles from './styles/GlobalStyles';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route />
-      </Routes>
-    </Router>
+    <>
+      <Helmet>
+        <link href='https://fonts.googleapis.com/css2?family=Actor&display=swap' rel='stylesheet' />
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Router>
+          <Routes>
+            <Route />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
