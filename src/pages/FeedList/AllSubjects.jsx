1f8305ea18481b7b96f891d7c84657e849495e7f
whdjh <whdjh98@naver.com>
2024-10-30 02:38:26 +0900

    feat: 카드리스트 구현완료(반응형 미완), 공통카드임포트

--- src/pages/FeedList/AllSubjects.jsx
+++ src/pages/FeedList/AllSubjects.jsx
@@ -3,6 +3,15 @@ import theme from '../../styles/theme';
 import { getSubjects } from '../../api/api';
 import React, { useEffect, useState } from 'react';
 import Dropdown from './Dropdown';
+import UserCard from '../../components/Card';
+
+const getPageSize = () => {
+  const width = window.innerWidth;
+  if (width < 768) return 6;
+  else if (width < 868) return 6;
+  else if (width < 1280) return 8;
+  else return 8;
+};
 
 const Container = styled.div`
   display: flex;
@@ -39,12 +48,56 @@ const Title = styled.p`
   }
 `;
 
+const GridContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: even;
+  gap: 1.5rem;
+`;
+
+const UserCardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
 function AllSubjects() {
+  const [pageSize, setPageSize] = useState(getPageSize());
+  const [subjectList, setSubjectList] = useState([]);
   const [orderBy, setOrderBy] = useState('createdAt');
+  const [page, setPage] = useState(1);
 
   const handleSortCard = (sortOption) => {
     setOrderBy(sortOption);
   };
+
+  const fetchData = async ({ orderBy, page, pageSize }) => {
+    const subjects = await getSubjects({ orderBy, page, pageSize });
+    setSubjectList(subjects.results);
+    // 페이지네이션자리
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPageSize(getPageSize());
+    };
+
+    window.addEventListener('resize', handleResize);
+    fetchData({ orderBy, page, pageSize });
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [orderBy, page, pageSize]);
 
   return (
     <Container>
@@ -52,6 +105,13 @@ function AllSubjects() {
         <Title>누구에게 질문할까요?</Title>
         <Dropdown onSortCard={handleSortCard} />
       </HeaderContainer>
+      <GridContainer>
+        <UserCardGrid>
+          {subjectList?.map((subject) => (
+            <UserCard item={subject} key={subject.id} />
+          ))}
+        </UserCardGrid>
+      </GridContainer>
     </Container>
   );
 }
