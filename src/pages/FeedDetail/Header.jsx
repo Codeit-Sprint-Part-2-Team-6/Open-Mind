1a9970a1d2b014ac159b78c8b399fa3b8ddeaf7c
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-29 19:21:05 +0900

    feat: URL 복사, facebook 공유 구현
    - URL복사 버튼 클릭시 클립보드에 현재 주소가 복사
    - URL이 복사되었다는 토스트 5초간 팝업
    - facebook 공유 버튼 클릭시 해당 공유 페이지로 이동

--- src/pages/FeedDetail/Header.jsx
+++ src/pages/FeedDetail/Header.jsx
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shareKakao } from '../../utills/KakaoShare';
+import { useState } from 'react';
 
 const HeaderContainer = styled.header`
   display: flex;
@@ -41,28 +42,73 @@ const ShareIconLink = styled.a`
   height: 40px;
 `;
 
+const CopyUrlBtn = styled.div`
+  width: 40px;
+  hgieht: 40px;
+  cursor: pointer;
+`;
+
 const ShareIcon = styled.img`
   width: 100%;
   height: 100%;
 `;
 
+const fadeInOut = keyframes`
+  0% { opacity: 0; bottom: 10px; }
+  10% { opacity: 1; bottom: 30px; }
+  90% { opacity: 1; bottom: 30px; }
+  100% { opacity: 0; bottom: 10px; }
+`;
+
+const ToastMessage = styled.div`
+  position: fixed;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0, 0, 0, 0.8);
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 0.875rem;
+  animation: ${fadeInOut} 5s ease-in-out forwards;
+  z-index: 1000;
+`;
+
 function Header() {
+  const [showToast, setShowToast] = useState(false);
+
+  const copyCurrentUrl = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 5000);
+    } catch (error) {
+      alert('URL 복사에 실패했습니다.');
+    }
+  };
+
   return (
     <HeaderContainer>
       <Logo src='/images/contents/logo.svg' />
       <ProfileImage src='/images/contents/profile.svg' />
       <UserName as='h2'>이동훈</UserName>
       <ShareContainer>
-        <ShareIconLink href='https://example.com/share'>
+        <CopyUrlBtn onClick={copyCurrentUrl}>
           <ShareIcon src='/images/icons/ic_share.svg' />
-        </ShareIconLink>
+        </CopyUrlBtn>
         <ShareIconLink onClick={shareKakao} href='/post'>
           <ShareIcon src='/images/icons/ic_kakao-share.svg' />
         </ShareIconLink>
-        <ShareIconLink href='https://facebook.com/share'>
-          <ShareIcon src='/images/icons/ic_facebook-share.svg' />
+        <ShareIconLink
+          href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <ShareIcon src='/images/icons/ic_facebook-share.svg' alt='페이스북 공유' />
         </ShareIconLink>
       </ShareContainer>
+
+      {showToast && <ToastMessage>URL이 복사되었습니다</ToastMessage>}
     </HeaderContainer>
   );
 }
