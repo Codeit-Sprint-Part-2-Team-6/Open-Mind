a8c82209a54c273b106352506bc571c390533beb
whdjh <whdjh98@naver.com>
2024-10-30 20:47:27 +0900

    fix: conflict충돌 예상코드 삭제

--- src/App.js
+++ src/App.js
@@ -3,23 +3,8 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
 import { Helmet } from 'react-helmet';
-<<<<<<<<< Temporary merge branch 1
 import FeedList from './pages/FeedList/FeedList';
-
-function AppContent() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  return (
-    <div>
-      {location.pathname !== '/list' && (
-        <button onClick={() => navigate('/list')}>Go to FeedList</button>
-      )}
-    </div>
-  );
-}
-=========
 import FeedDetailPage from './pages/FeedDetail/FeedDetailPage';
->>>>>>>>> Temporary merge branch 2
 
 function App() {
   return (
@@ -27,16 +12,13 @@ function App() {
       <Helmet>
         <link href='https://fonts.googleapis.com/css2?family=Actor&display=swap' rel='stylesheet' />
       </Helmet>
-    
+
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Router>
           <Routes>
-<<<<<<<<< Temporary merge branch 1
             <Route path='list' element={<FeedList />} />
-=========
             <Route path='/post' element={<FeedDetailPage />} />
->>>>>>>>> Temporary merge branch 2
           </Routes>
         </Router>
       </ThemeProvider>
