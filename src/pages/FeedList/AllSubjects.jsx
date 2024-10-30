d4fb0dc6cd8b9edb23f0e56ce370ac529798900b
whdjh <whdjh98@naver.com>
2024-10-30 20:03:07 +0900

    feat: 카드리스트 UI구성 완료(추후 페이지네이션버튼작동만들기)

--- src/pages/FeedList/AllSubjects.jsx
+++ src/pages/FeedList/AllSubjects.jsx
@@ -4,9 +4,11 @@ import { getSubjects } from '../../api/api';
 import React, { useEffect, useState } from 'react';
 import Dropdown from './Dropdown';
 import UserCard from '../../components/Card';
+import Pagination from './Pagination';
 
 const getPageSize = () => {
   const width = window.innerWidth;
+
   if (width < 768) return 6;
   else if (width < 868) return 6;
   else if (width < 1280) return 8;
@@ -52,7 +54,7 @@ const GridContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: even;
+  justify-content: space-evenly;
   gap: 1.5rem;
 `;
 
@@ -61,12 +63,33 @@ const UserCardGrid = styled.div`
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
 
-  @media (min-width: 768px) {
+  @media ${theme.typography.device.tabletMn} {
     grid-template-columns: repeat(3, 1fr);
   }
 
-  @media (min-width: 1024px) {
+  @media (min-width: 950px) {
     grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media ${theme.typography.device.laptopMn} {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+const PaginationContainer = styled.div`
+  position: absolute;
+  top: 820px;
+
+  @media ${theme.typography.device.tabletMn} {
+    top: 718px;
+  }
+
+  @media (min-width: 950px) {
+    top: 701px;
+  }
+
+  @media ${theme.typography.device.laptopMn} {
+    top: 695px;
   }
 `;
 
@@ -75,15 +98,17 @@ function AllSubjects() {
   const [subjectList, setSubjectList] = useState([]);
   const [orderBy, setOrderBy] = useState('createdAt');
   const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState();
 
   const handleSortCard = (sortOption) => {
     setOrderBy(sortOption);
+    setPage(1);
   };
 
   const fetchData = async ({ orderBy, page, pageSize }) => {
     const subjects = await getSubjects({ orderBy, page, pageSize });
     setSubjectList(subjects.results);
-    // 페이지네이션자리
+    setTotalPage(Math.ceil(subjects.count / pageSize));
   };
 
   useEffect(() => {
@@ -99,11 +124,16 @@ function AllSubjects() {
     };
   }, [orderBy, page, pageSize]);
 
+  const pageChange = (pageNum) => {
+    console.log('page change', pageNum);
+    setPage(pageNum);
+  };
+
   return (
     <Container>
       <HeaderContainer>
         <Title>누구에게 질문할까요?</Title>
-        <Dropdown onSortCard={handleSortCard} />
+        <Dropdown onSorCard={handleSortCard} />
       </HeaderContainer>
       <GridContainer>
         <UserCardGrid>
@@ -111,6 +141,9 @@ function AllSubjects() {
             <UserCard item={subject} key={subject.id} />
           ))}
         </UserCardGrid>
+        <PaginationContainer>
+          <Pagination totalPage={totalPage} currentPage={page} pageChange={pageChange} />
+        </PaginationContainer>
       </GridContainer>
     </Container>
   );
