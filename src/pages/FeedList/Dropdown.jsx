faccd0ffa9da13c70ad55c2e591d4cc5a02bd5e2
whdjh <whdjh98@naver.com>
2024-10-31 23:17:10 +0900

    feat: 드롭다운 스타일 추가

--- src/pages/FeedList/Dropdown.jsx
+++ src/pages/FeedList/Dropdown.jsx
@@ -1,6 +1,55 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const DropdownContainer = styled.div`
+  height: 34px;
+  margin-top: 20px;
+  border: 1px solid var(--Grayscale-60);
+  background-color: var(--Grayscale-10);
+  border-radius: 8px;
+  font-size: 16px;
+  font-weight: 100;
+`;
+
+const DropdownButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  justify-content: center;
+  padding: 5px 12px;
+  cursor: pointer;
+  background: none;
+  border-color: black;
+  border-radius: 8px;
+  font-size: inherit;
+  font-weight: inherit;
+
+  img {
+    width: 16px;
+    height: 16px;
+    transition: transform 0.3s;
+    transform: ${({ isDropDown }) => (isDropDown ? 'rotate(180deg)' : 'rotate(0)')};
+  }
+`;
+
+const DropdownList = styled.div`
+  margin-top: 4px;
+  height: 64px;
+  border: 1px solid #8c8c8c;
+  background-color: var(--Grayscale-10);
+  border-radius: 8px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+const DropdownOption = styled.div`
+  height: 28px;
+  padding: 8px;
+  text-align: center;
+`;
+
 function Dropdown({ onSortCard }) {
   const [isDropDown, setIsDropDown] = useState(false);
   const [orderBy, setOrderBy] = useState('최신순');
@@ -19,22 +68,28 @@ function Dropdown({ onSortCard }) {
   };
 
   return (
-    <div>
-      <button onClick={toggleDropdown}>
+    <DropdownContainer>
+      <DropdownButton onClick={toggleDropdown}>
         {orderBy}
-        <img src='images/icons/Arrow-up.svg' alt='드롭다운 토글' />
-      </button>
+        <img
+          src={isDropDown ? 'images/icons/Arrow-up.svg' : 'images/icons/Arrow-down.svg'}
+          alt={isDropDown ? '화살표위' : '화살표아래'}
+        />
+      </DropdownButton>
 
       {isDropDown && (
-        <div>
+        <DropdownList>
           {sortingOptions.map((option) => (
-            <div key={option.value} onClick={() => handleSortCard(option.value, option.label)} >
+            <DropdownOption
+              key={option.value}
+              onClick={() => handleSortCard(option.value, option.label)}
+            >
               {option.label}
-            </div>
+            </DropdownOption>
           ))}
-        </div>
+        </DropdownList>
       )}
-    </div>
+    </DropdownContainer>
   );
 }
 
