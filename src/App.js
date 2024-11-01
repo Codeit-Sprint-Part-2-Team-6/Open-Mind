ebe45ae2ccc89341452de4b242f5e13b66b52783
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-01 21:38:42 +0900

    feat : Pretendard 다이나믹 서브셋으로 재설정, Light,Dark모드 기능추가
    - Helmet에 재설정
    - ToggleTheme.jsx추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
+import { lightTheme, darkTheme } from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
 import { Helmet } from 'react-helmet';
 import HomePage from './pages/Home/Homepage';
 import FeedList from './pages/FeedList/FeedList';
 import FeedDetailPage from './pages/FeedDetail/FeedDetailPage';
+import ToggleThemeBtn from './components/ToggleTheme';
+import { useState } from 'react';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
   return (
     <>
       <Helmet>
         <link href='https://fonts.googleapis.com/css2?family=Actor&display=swap' rel='stylesheet' />
+        <link
+          rel='stylesheet'
+          as='style'
+          crossorigin
+          href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css'
+        />
       </Helmet>
 
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <GlobalStyles />
+        <ToggleThemeBtn isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         <Router>
           <Routes>
             <Route index element={<HomePage />} />
