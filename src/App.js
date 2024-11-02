441206ed615ca965b679984d66bbcdaa1d8f020f
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-02 17:30:04 +0900

    feat : HelmetProvider로 모든 컴퍼넌트에 meta 정의

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import HomePage from './pages/Home/Homepage';
 import FeedList from './pages/FeedList/FeedList';
 import FeedDetailPage from './pages/FeedDetail/FeedDetailPage';
@@ -15,9 +15,29 @@ function App() {
   const toggleTheme = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
+
   return (
-    <>
+    <HelmetProvider>
       <Helmet>
+        <title>11-6팀 오픈마인드</title>
+        <meta name='keywords' content='기초프로젝트, 프론트앤드 11기 6팀, 오픈마인드, 코드잇' />
+        <meta
+          name='description'
+          content='익명성을 보장하며 CRUD(Create, Read, Update, Delete) 기능을 구현하는 웹 애플리케이션입니다.'
+        />
+        <meta property='og:site_name' content='11-6팀 오픈마인드' />
+        <meta property='og:title' content='11-6팀 오픈마인드' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:description'
+          content='익명성을 보장하며 CRUD(Create, Read, Update, Delete) 기능을 구현하는 웹 애플리케이션입니다.'
+        />
+        <meta
+          property='og:image'
+          content='https://open-mind-team-6.netlify.app/public/images/meta/SumMeta.png'
+        />
+        <meta property='og:url' content='https://open-mind-team-6.netlify.app' />
+        <link rel='icon' href='/images/meta/favicon.ico' type='image/x-icon' />
         <link href='https://fonts.googleapis.com/css2?family=Actor&display=swap' rel='stylesheet' />
         <link
           rel='stylesheet'
@@ -39,7 +59,7 @@ function App() {
           </Routes>
         </Router>
       </ThemeProvider>
-    </>
+    </HelmetProvider>
   );
 }
 
