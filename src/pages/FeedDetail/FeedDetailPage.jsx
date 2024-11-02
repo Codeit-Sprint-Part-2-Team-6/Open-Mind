a21606008c10299f3c9c08eb78694c8b05b547e3
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 11:47:06 +0900

    refactor: 카카오 SDK를 공유 버튼 클릭시 로드 되도록 수정
    - index.html에서 스크립트를 제거 후, 버튼 클릭시 동적으로 설치
    - 카카오톡 공유 메시지 템플릿 수정

--- src/pages/FeedDetail/FeedDetailPage.jsx
+++ src/pages/FeedDetail/FeedDetailPage.jsx
@@ -146,7 +146,12 @@ function FeedDetailPage() {
 
   return (
     <>
-      <Header image={subject.imageSource} name={subject.name} />
+      <Header
+        id={id}
+        image={subject.imageSource}
+        name={subject.name}
+        questionsCount={questionsCount}
+      />
       <Main>
         <QuestionsContainer>
           {questionsCount ? (
